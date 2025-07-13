2f09aa8c9b25b21303fdaa9f704aa8ae1ea5e8a3
nitin <nitinsain4811@gmail.com>
2025-07-13 17:59:02 +0530

    third

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ from flask import Flask
 app=Flask(__name__)
 
 @app.route("/")
-
 def home():
     return "hello from Flask deployed via jenkins & Docker!"
 
-if__name__ == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
 
